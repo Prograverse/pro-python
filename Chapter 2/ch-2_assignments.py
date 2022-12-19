61efa7574b4c66d28cf6e3c90b5786adc195719a
Prograverse <120959557+Prograverse@users.noreply.github.com>
2022-12-19 22:00:42 +0530

    Add files via upload

---
+++ Chapter 2/ch-2_assignments.py
@@ -0,0 +1,69 @@
+#assignment 1 
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+a = int(a)
+b = int(b)
+
+sum = a+b
+
+print("The sum is: ", sum)
+
+
+#assignment 2
+
+a = input("Enter the number: ")
+a = int(a)
+
+square = a*a
+
+print("Square = ", square)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(12)
+print(35.5)
